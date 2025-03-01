912268765cbb8a6f18831e44c940cec86afd823f
Ting Rong You <ryting999@gmail.com>
2025-03-01 13:48:19 +0800

    Version 0

---
+++ src/assas/computers/Staff.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package assas.computers;
+
+/**
+ *
+ * @author Acer
+ */
+public class Staff extends User{
+    
+    public Staff(String name, String email, String password, String contactNum) {
+            this.name = name;
+            this.email = email;
+            this.password = password;
+            this.contactNum = contactNum;
+        }
+    
+}
---
+++ src/assas/computers/User.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package assas.computers;
+
+/**
+ *
+ * @author Acer
+ */
+public abstract class User {
+    String name;
+    String email;
+    String password;
+    String contactNum;
+    
+    public void registration(String email, String password) {
+    
+}
+    
+}
