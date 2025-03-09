dab4181f07192443ba436d71c50361d5e70cc7ca
Jasden0517 <yongchongxin0517@gmail.com>
2025-03-09 21:09:00 +0800

    Registration V1 updated

--- src/assas/computers/User.java
+++ src/assas/computers/User.java
@@ -37,6 +37,7 @@ public class User {
             this.email = email;
             this.password = password;
             
+            //Read CustomerAcc.txt
             try(BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
                 String line;
                 
@@ -44,80 +45,89 @@ public class User {
                 String[] words = line.split(";"); // Assuming the file format is: name;email;password;phone;address
 
                 if (words[1].equals(this.email) && words[2].equals(this.password)) { // FIXED: Use .equals()
-                    System.out.println("User already registered. Invalid registration.");
+                    System.out.println("Error: User already registered. Invalid registration.");
                     break;
                 }
-            }
-            
+            }  
         }  
         catch(FileNotFoundException e){
-            System.out.println("Cannot locate the file");
+            System.out.println("Error: Cannot locate the file");
             
         }
         catch(IOException e){
-            System.out.println("Cannot read the file!");
+            System.out.println("Error: Cannot read the file!");
         }
+    }
     
-}
   public static boolean usernameValidate(String username){
-      boolean validity = true;
-      if(username.length() <= 20){
-          validity = true; 
+      //Validate the length of username, length from 1 to 20
+      if(username.length() >0 && username.length() <= 20){
+          return true; 
       }
+      //Error message if the username does not fit the required length
       else{
-          validity = false;
-      }
-      return validity;   
-      
+          System.out.println("Error: Your Username should be between 1 - 20 !! ");
+          return false;   
+      }   
   }
   
   public static boolean emailValidate(String email){
-      boolean validity = true;
+      //Validate the email format, whether including both of @ and .com
       if((email.contains("@")) && (email.contains(".com"))){
-          validity = true;
+          return true;
       }
+      //Error message if does not align the format
       else{
-          validity =false;
+          System.out.println("Error: Your email should include @ and .com !!");
+          return false;
       }
-      return validity;
   }
   
   public static boolean phoneNumValidate(String phoneNum ){
-      boolean validity = true;
+      //Validate the phone number, starts with 01, and at most 10 to 11 number
       if(phoneNum.matches("^01\\d{8,9}$")){
-          validity = true;
+          return true;
       }
+      //Error message if not meet the format
       else{
-          validity = false;
+          System.out.println("Error: Your phone number should start be in 01xxxxxxxx and with 10 or 11 number only !!");
+          return false;
       }
-      return validity; 
   }
+  
+  
   public static boolean passwordValidate(String password){
       boolean hasUpper = false;
       boolean hasLower = false;
       boolean hasDigit = false;
       boolean hasSpecial = false;
-      boolean sufficientLength = true;
-      boolean validity = true;
+      boolean sufficientLength = false;
       char[] specialCharacters = {'!', '@', '#', '%', '*', '&', '$'};
       
-      if(password.length()<8 || password.length() >15){
-          sufficientLength = false;   
+      //Validate the length of password, from 8 to 15
+      if(password.length()>=8 && password.length() <=15){
+          sufficientLength = true;   
       }
       else{
-          sufficientLength = true;
+          System.out.println("Error: Your password should within 8 to 15 characters");
+          sufficientLength = false;
       }
       
+     //Loop the password,and validate
       for(char ch: password.toCharArray()){
+          //Validate whether has at least one Uppercase Letter
           if(Character.isUpperCase(ch)){
               hasUpper = true;  
           }
+          //Validate whether has at least one Lowerrcase Letter
           else if(Character.isLowerCase(ch)){
               hasLower = true;
           }
+          //Validate whether has at least one Digit
           else if(Character.isDigit(ch)){
               hasDigit = true;
           }
+          //Validate whether has at least one Special Character
           else{
                   for(char special : specialCharacters){
                       if(ch == special){
@@ -127,16 +137,38 @@ public class User {
                   }   
           }   
       }
+      
+      //Error message if does not meet the password format
+      if(!hasUpper){
+          System.out.println("Error: Password must contain at least one uppercase letter!");
+      }
+      if(!hasLower){
+          System.out.println("Error: Password must contain at least one lowercase letter!");
+      }
+      if(!hasDigit){
+          System.out.println("Error: Password must contain at least one digit!");
+      }
+      if(!hasSpecial){
+          System.out.println("Error: Password must contain at least one special character (@, #, $, %, &, *, !, ?)");
+      }
+      
+      
       return hasUpper && hasLower && hasDigit && hasSpecial && sufficientLength;
   
   }
   
   public static boolean addressCheck(String deliveryAddress){
-      boolean validity = true;
+      //Trim white space
+      deliveryAddress = deliveryAddress.trim();
+    
+      //Validate Length of Delivery Address, from length of 1 to 50
       if(deliveryAddress.length() > 0 && deliveryAddress.length()<=50){
-          validity = true; 
+          return true; 
       }
-      return validity;
+      else{
+          System.out.println("Error: Your Delivery Address should be in length of between 1 to 50");
+          return false;
+      }
   }
 }
 
